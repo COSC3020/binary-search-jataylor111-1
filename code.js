950b3ae50d57fd7544589e03307557d9857af4fe
jataylor111 <143449478+jataylor111@users.noreply.github.com>
2024-08-29 16:10:58 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
 function binarySearch(list, element) {
+    //Skip the search if the list is empty
+    if (list.length === 0) { return -1}
+    
+    //Now search
+    let left = 0;
+    let right = list.length - 1;
+
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2);
+        if (list[middle] < element) {
+            left = middle + 1;
+        } else if (list[middle] > element) {
+            right = middle - 1;
+        } else {
+            return 0;
+        }
+    }
+
     return -1;
 }
